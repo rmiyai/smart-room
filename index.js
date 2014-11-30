c4e8346a2a266e02018c49759eef1d8920075fa7
Ryuji Miyai <ryuji.bump@gmail.com>
2014-11-30 19:43:27 +0900

    新規HTML追加

--- index.js
+++ index.js
@@ -44,6 +44,12 @@ io.sockets.on('connection',function(socket){
 	});
 });
 
+io.sockets.on('connection',function(socket){
+    socket.on('history',function(data){
+        console.log(data);
+    });
+});
+
 
 //グラフ描画用
 var WsServer = require('ws').Server;
